9d86a08eb386360ef0c3a05b6b9705587c997787
hunterbounter_dev <emin.sargin@arjeta.com.tr>
2024-07-14 18:24:14 +0300

    passhass and env added.

---
+++ cmd/hunterbounter-panel/config.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+// ENVIRONMENT VARIABLES
+type EnvirontmentVariables struct {
+	DBUsername string
+	DBPassword string
+	DBHost     string
+	DBPort     string
+	DBName     string
+	DBParams   string
+	DBType     string
+}
+
+func InitEnv() EnvirontmentVariables {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	return EnvirontmentVariables{
+		DBUsername: os.Getenv("DB_USERNAME"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBName:     os.Getenv("DB_NAME"),
+		DBParams:   os.Getenv("DB_PARAMS"),
+		DBType:     os.Getenv("DB_TYPE"),
+	}
+}
