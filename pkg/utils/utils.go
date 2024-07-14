edd916dde490e5b8d09a1b40e391378538680a79
hunterbounter_dev <emin.sargin@arjeta.com.tr>
2024-07-14 19:01:18 +0300

    utils.go new methods

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,10 +1,29 @@
 package utils
 
 import (
+	"fmt"
 	"html"
+	"log"
+	"os"
+	"path/filepath"
 	"regexp"
+	"strconv"
 	"time"
 )
+
+// Number to string
+func AnyToString(number interface{}) string {
+	return fmt.Sprintf("%v", number)
+}
+
+func AnyToInt(number interface{}) int {
+	// fmt.Sprintf converts string to int
+	i, err := strconv.Atoi(AnyToString(number))
+	if err != nil {
+		return 0 // Hata varsa, 0 döner
+	}
+	return i // if conversion is successful, return the converted number
+}
 
 func TimeToString(t time.Time) string {
 	return t.Format("2006-01-02 15:04:05")
@@ -42,4 +61,25 @@ func GetString(value interface{}) string {
 	}
 
 	return str
+}
+
+func RunningDir() string {
+
+	path, err := os.Executable()
+	if err != nil {
+		return ""
+	}
+
+	//log.Println("Executable Path : ", path)
+
+	// the executable directory
+	exPath := filepath.Dir(path)
+
+	//log.Println("Executable Path : ", exPath)
+
+	exPath = exPath + "/../../"
+
+	log.Println("RunningDir: ", exPath)
+
+	return exPath
 }
