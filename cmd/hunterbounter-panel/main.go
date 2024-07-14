42fdb859eb554820b00e0c764e233516387ab257
hunterbounter_dev <emin.sargin@arjeta.com.tr>
2024-07-14 19:00:37 +0300

    env was enabled to start with

--- cmd/hunterbounter-panel/main.go
+++ cmd/hunterbounter-panel/main.go
@@ -2,31 +2,18 @@ package main
 
 import (
 	"fmt"
-	"hunterbounter.com/web-panel/pkg/database"
 	"hunterbounter.com/web-panel/pkg/env"
 	"hunterbounter.com/web-panel/web/api/bootstrap"
 	"log"
 )
 
 func init() {
-	return
-	postgres_info := database.PostgreSQLInfo{
-		Username:  "zap_user",
-		Password:  "your_password",
-		Hostname:  "127.0.0.1",
-		Port:      5432,
-		Name:      "postgres",
-		Parameter: "sslmode=disable",
-	}
+	InitSys()
 
-	info := database.Info{
-		Type:       "PostgreSQL",
-		PostgreSQL: postgres_info,
-	}
-	database.Connect(info)
 }
 
 func main() {
+
 	log.Println("Running HunterBounter Web Panel...")
 	panelApp := bootstrap.HunterBounterWeb()
 	err := panelApp.Listen(fmt.Sprintf("%s:%s", env.GetEnv("APP_HOST", "0.0.0.0"), env.GetEnv("APP_PORT", "9000")))
