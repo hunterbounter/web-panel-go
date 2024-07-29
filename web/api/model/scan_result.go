adfe84bd4371099b7ee5b502c402fdcf5f655b11
hunterbounter_dev <emin.sargin@arjeta.com.tr>
2024-07-29 23:14:42 +0300

    nuclei development

--- web/api/model/scan_result.go
+++ web/api/model/scan_result.go
@@ -1,12 +1,26 @@
 package model
 
-import "hunterbounter.com/web-panel/pkg/database"
+import (
+	"hunterbounter.com/web-panel/pkg/database"
+	"log"
+)
 
 func GetTotalReportCount() int {
 
 	dbRecords, err := database.Select("zap_scan_results", map[string]interface{}{})
 	if err != nil {
-		return 0
+		log.Println("Error while checking record is exist", err)
 	}
-	return len(dbRecords)
+
+	dbRecords2, err2 := database.Select("openvas_scan_result", map[string]interface{}{})
+	if err2 != nil {
+		log.Println("Error while checking record is exist", err2)
+	}
+
+	dbRecords3, err3 := database.Select("nuclei_scan_results", map[string]interface{}{})
+	if err2 != nil {
+		log.Println("Error while checking record is exist", err3)
+	}
+
+	return len(dbRecords) + len(dbRecords2) + len(dbRecords3)
 }
