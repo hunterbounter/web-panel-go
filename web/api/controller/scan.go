6f4684345e8273b6e0b04ab618585cb5eba14017
hunterbounter_dev <emin.sargin@arjeta.com.tr>
2024-08-01 03:32:33 +0300

    algo update

--- web/api/controller/scan.go
+++ web/api/controller/scan.go
@@ -8,89 +8,118 @@ import (
 	"log"
 )
 
-const DOCKER_DOMAIN_IMAGE_NAME = "hunter_bounter_zapv1"
+const DOCKER_DOMAIN_IMAGE_NAME_ZAP = "hunter_bounter_zapv1"
+const DOCKER_DOMAIN_IMAGE_NAME_OPENVAS = "hunterbounter:openvasv1"
+const DOCKER_DOMAIN_IMAGE_NAME_NUCLEI = "hunterbounter:nucleiv1"
 
 const maxContainers = 5
 const targetsPerContainer = 10
 
 func StartScan(c *fiber.Ctx) error {
+	log.Println("Starting scan...")
 
 	// First find total waiting scan count
-
 	var zapWaitingScanCount = model.GetWaitingZAPTargetDomainCount()
-	log.Println("ZAP Waiting Scan Count", zapWaitingScanCount)
+	log.Println("ZAP Waiting Scan Count:", zapWaitingScanCount)
 	var openvasWaitingScanCount = model.GetWaitingOpenVASTargetDomainCount()
-	log.Println("OpenVAS Waiting Scan Count", openvasWaitingScanCount)
-
-	var totalWaitingScanCount = zapWaitingScanCount + openvasWaitingScanCount
-
-	requiredContainers := (totalWaitingScanCount + targetsPerContainer - 1) / targetsPerContainer // Round up to the nearest whole number
-
-	if requiredContainers > maxContainers {
-		requiredContainers = maxContainers
-	}
+	log.Println("OpenVAS Waiting Scan Count:", openvasWaitingScanCount)
+	var nucleiWaitingScanCount = model.GetWaitingNucleiTargetDomainCount()
+	log.Println("Nuclei Waiting Scan Count:", nucleiWaitingScanCount)
 
 	dockerManager := hunterbounter_docker.NewDockerManager()
 
-	containers, err := dockerManager.ListRunningContainersViaImageName(DOCKER_DOMAIN_IMAGE_NAME) // List all running containers
-
-	if err != nil {
-		log.Println("Error while listing running containers", err)
-		return c.JSON(hunterbounter_response.HunterBounterResponse(false, "Error while listing running containers", nil))
-	}
-
-	// If the number of running containers is less than the required number of containers, start new containers until it is equalized
-	log.Println("Total running containers", len(containers))
-	log.Println("Required container count", requiredContainers)
-	if len(containers) < requiredContainers {
-		zapContainersToStart := (zapWaitingScanCount + targetsPerContainer - 1) / targetsPerContainer
-		log.Println("zapContainersToStart", zapContainersToStart)
-		openvasContainersToStart := (openvasWaitingScanCount + targetsPerContainer - 1) / targetsPerContainer
-		log.Println("openvasContainersToStart", openvasContainersToStart)
-		for i := 0; i < zapContainersToStart; i++ {
-			imageName := "hunter_bounter_zapv1"
-			user := "root"
-			port := "5002:5002"
-			dns := "1.1.1.1"
-
-			containerID, err := dockerManager.RunContainer(imageName, user, port, dns)
-			log.Println("Running ZAP container ID", containerID)
-			if err != nil {
-				log.Println("Error while starting ZAP container", err)
-				return c.JSON(hunterbounter_response.HunterBounterResponse(false, "Error while starting ZAP container", nil))
-			}
-		}
-
-		for i := 0; i < openvasContainersToStart; i++ {
-			imageName := "hunterbounter:openvasv1"
-
-			containerID, err := dockerManager.RunContainerNoCommand(imageName)
-			log.Println("Running OpenVAS container ID", containerID)
-			if err != nil {
-				log.Println("Error while starting OpenVAS container", err)
-				return c.JSON(hunterbounter_response.HunterBounterResponse(false, "Error while starting OpenVAS container", nil))
-			}
+	// Function to remove a container by ID
+	removeContainer := func(containerID string) {
+		err := dockerManager.RemoveContainer(containerID)
+		if err != nil {
+			log.Println("Error while removing container", containerID, ":", err)
+		} else {
+			log.Println("Successfully removed container", containerID)
 		}
 	}
 
-	lastContainersCount, err := dockerManager.ListRunningContainersViaImageName(DOCKER_DOMAIN_IMAGE_NAME) // List all running containers
-	if err != nil {
-		log.Println("Error while listing running containers", err)
-		return c.JSON(hunterbounter_response.HunterBounterResponse(false, "Error while listing running containers", nil))
+	// Function to adjust containers for an agent
+	adjustContainers := func(agentName string, waitingCount int) {
+		log.Printf("Adjusting containers for agent %s...", agentName)
+		runningContainers, err := dockerManager.ListRunningContainersViaImageName(agentName)
+		if err != nil {
+			log.Printf("Error while listing running containers for %s: %v", agentName, err)
+			return
+		}
+		runningCount := len(runningContainers)
+		requiredContainers := (waitingCount + targetsPerContainer - 1) / targetsPerContainer // Round up to the nearest whole number
+		log.Printf("%s - Running: %d, Required: %d", agentName, runningCount, requiredContainers)
+
+		if requiredContainers > maxContainers {
+			requiredContainers = maxContainers
+		}
+
+		if runningCount > requiredContainers {
+			containersToStop := runningCount - requiredContainers
+			log.Printf("Stopping %d containers for %s", containersToStop, agentName)
+			for i := 0; i < containersToStop; i++ {
+				removeContainer(runningContainers[i])
+			}
+		} else if runningCount < requiredContainers {
+			containersToStart := requiredContainers - runningCount
+			log.Printf("Starting %d containers for %s", containersToStart, agentName)
+			for i := 0; i < containersToStart; i++ {
+				var imageName string
+				switch agentName {
+				case DOCKER_DOMAIN_IMAGE_NAME_ZAP:
+					imageName = "hunter_bounter_zapv1"
+				case DOCKER_DOMAIN_IMAGE_NAME_OPENVAS:
+					imageName = "hunterbounter:openvasv1"
+				case DOCKER_DOMAIN_IMAGE_NAME_NUCLEI:
+					imageName = "hunterbounter:nucleiv1"
+				}
+
+				if agentName == DOCKER_DOMAIN_IMAGE_NAME_ZAP {
+					user := "root"
+					port := "5002:5002"
+					dns := "1.1.1.1"
+					log.Println("Starting ZAP container...")
+					containerID, err := dockerManager.RunContainer(imageName, user, port, dns)
+					log.Println("Running ZAP container ID:", containerID)
+					if err != nil {
+						log.Println("Error while starting ZAP container:", err)
+						return
+					}
+				} else {
+					log.Printf("Starting container for %s...", agentName)
+					containerID, err := dockerManager.RunContainerNoCommand(imageName)
+					log.Printf("Running %s container ID: %s", agentName, containerID)
+					if err != nil {
+						log.Printf("Error while starting %s container: %v", agentName, err)
+						return
+					}
+				}
+			}
+		}
 	}
 
-	log.Println("last container count", len(lastContainersCount))
+	// Adjust containers for each agent
+	adjustContainers(DOCKER_DOMAIN_IMAGE_NAME_ZAP, zapWaitingScanCount)
+	adjustContainers(DOCKER_DOMAIN_IMAGE_NAME_OPENVAS, openvasWaitingScanCount)
+	adjustContainers(DOCKER_DOMAIN_IMAGE_NAME_NUCLEI, nucleiWaitingScanCount)
 
-	if err != nil {
-		log.Println("Error while listing containers", err)
-		return c.JSON(hunterbounter_response.HunterBounterResponse(false, "Error while listing containers", nil))
-	}
-	log.Println("Total running containers", len(containers))
+	// Log the final state
+	var zapLen, _ = dockerManager.ListRunningContainersViaImageName(DOCKER_DOMAIN_IMAGE_NAME_ZAP)
+	var openvasLen, _ = dockerManager.ListRunningContainersViaImageName(DOCKER_DOMAIN_IMAGE_NAME_OPENVAS)
+	var nucleiLen, _ = dockerManager.ListRunningContainersViaImageName(DOCKER_DOMAIN_IMAGE_NAME_NUCLEI)
+
+	log.Println("Final state after adjustments:")
+	log.Println("ZAP running containers:", zapLen)
+	log.Println("OpenVAS running containers:", openvasLen)
+	log.Println("Nuclei running containers:", nucleiLen)
 
 	// Type 1 Domain
 	// Status 0 Added
+	log.Println("Updating target statuses...")
 	model.UpdateTargetsStatus(0, 1, 1)
 	model.UpdateTargetsStatus(0, 2, 1)
+	model.UpdateTargetsStatus(0, 3, 1)
 
-	return c.JSON(hunterbounter_response.HunterBounterResponse(true, "Scan started", totalWaitingScanCount))
+	log.Println("Scan started successfully.")
+	return c.JSON(hunterbounter_response.HunterBounterResponse(true, "Scan started", nil))
 }
